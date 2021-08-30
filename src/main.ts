784b870f639771b2063c393ac7de2e0e7051d720
youldali <y.ouldali@gmail.com>
2021-08-30 14:55:23 +0200

    feat(composition-action): release as composite action

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,5 @@
-import * as core from '@actions/core';
-import {exec} from '@actions/exec';
-
 async function run(): Promise<void> {
-  try {
-    core.info('\nInstalling dependencies...');
-    await installDependencies();
-
-    core.info('\nBuilding...');
-    await build();
-
-    core.info('\nPublishing to npm...');
-    await publish();
-
-    core.info('\nAll good !');
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
-async function build(): Promise<number> {
-  return exec('yarn run build');
-}
-
-async function publish(): Promise<number> {
-  return exec('yarn publish');
-}
-
-async function installDependencies(): Promise<number> {
-  return exec('yarn install --frozen-lockfile');
+  console.log('running');
 }
 
 run();
